e735dd7de8e93a1290b71eb62fd64ec4590a0c0f
Elton Tang <etang315@gmail.com>
2015-10-04 21:07:02 -0500

    Update install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2
-sudo apt-get install -y git
+sudo apt-get install -y apache2 git
 
 echo "Hello!" > /tmp/hello.txt
-git clone https://github.com/etango/Application-setup.git > /var/www/html
+
+git clone https://github.com/etango/Application-setup.git 
+
+mv ./Application-setup/page1.html /var/www/html/
+mv ./Application-setup/page2.html /var/www/html/
 
 
 
